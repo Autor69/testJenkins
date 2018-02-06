c4a2741580b9d9db7a1536563364e9236ec0872a
Autor69 <pisklecz@gmail.com>
2018-02-06 11:05:08 +0100

    Basic Class test

---
+++ tests/UserTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martinpistak
+ * Date: 06/02/2018
+ * Time: 11:03
+ */
+
+
+class UserTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testGetId()
+    {
+        $this->assertEquals('1', '1');
+
+    }
+
+    public function testSetId()
+    {
+
+    }
+
+    public function testGetPassword()
+    {
+
+    }
+
+    public function testSetPassword()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testChangePassword()
+    {
+
+    }
+}
